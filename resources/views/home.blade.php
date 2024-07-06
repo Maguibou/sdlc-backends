8e2d268ab11cdf5ad35e47e4a397ad33d8cd1b63
Maguibou <159534525+Maguibou@users.noreply.github.com>
2024-07-06 15:47:41 +0100

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,13 @@
 
 @section('content')
     <div class="jumbotron">
-        <h1 class="display-4">Bienvenue sur la Plateforme Mobile d’Aide aux Étrangers</h1>
+        <h1 class="display-4">Bienvenue sur la Plateforme Mobile d’Aide aux Étrangerjjjfs</h1>
+        <h1 class="display-4">Bienvenue sur la Plateforme Mobile d’Aide aux Étrangerjjjfs</h1>
+
+        <h1 class="display-4">Bienvenue sur la Plateforme Mobile d’Aide aux Étrangerjjjfs</h1>
+
+        <h1 class="display-4">Bienvenue sur la Plateforme Mobile d’Aide aux Étrangerjjjfs</h1>
+
         <p class="lead">Souscrivez à un abonnement et découvrez nos services pour faciliter votre intégration.</p>
         <hr class="my-4">
         <p class="lead">
